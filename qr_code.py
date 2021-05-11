3fe28e30eca848bd50b0749d8f128f0ed6d70ca2
Gisa Kaze Fredson <fredson.coder@gmail.com>
2021-05-11 18:57:02 +0200

    Reading qrcodes in form of image

--- qr_code.py
+++ qr_code.py
@@ -1,5 +1,5 @@
+# From line 2 to 11 it deals with creating/generating qrcode in form image
 import qrcode
-
 img = qrcode.make("https://fredson.netlify.app/")
 img.save("./Images/Portfolio.jpg")
 
@@ -9,3 +9,12 @@ img = qrcode.make('''
                   ''')
 
 img.save("./Images/President.jpg")
+# End generating Qrcodes in form of image
+
+# From line 15 to 19 it deals with reading qrcode from images and printing them
+import cv2
+
+detect = cv2.QRCodeDetector()
+val, points, straight_qrcode =   detect.detectAndDecode(cv2.imread("./Images/Portfolio.jpg"))
+print(val)
+# End reading Qrcodes in form of image
